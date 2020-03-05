b15a72823e6ba8806bab5c59399c7eb6953f09e7
rolandlee0308 <mlee38@mail.sfsu.edu>
2020-03-05 14:43:40 -0800

    DAO update (not tested)

--- src/main/java/PaymentDAO.java
+++ src/main/java/PaymentDAO.java
@@ -1,14 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
 public class PaymentDAO {
     private static PaymentDAO Instance;
 
-    public static PaymentDAO getInstance(){
+    private List<PaymentDTO> database = new ArrayList<>();
+
+    public List<PaymentDTO> getInstance(){
         if(Instance == null){
             Instance = new PaymentDAO();
         }
-        return Instance;
+        return database;
     }
 
-    private PaymentDAO(){
+    public void save(PaymentDTO data){
+        database.add(data);
+    }
 
+    public void delete(PaymentDTO data){
+        database.remove(data);
     }
 }
